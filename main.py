ad6e9a8dc606f59e964a163c2ae0e5045123d8d6
Andy Huang <73412379+goodrole@users.noreply.github.com>
2021-08-22 17:40:48 -0700

    Update main.py

--- main.py
+++ main.py
@@ -47,4 +47,4 @@ for filename in os.listdir('./cogs'):
         client.load_extension(f'cogs.{filename[:-3]}')
 
 if __name__ == '__main__':
-    client.run(token)
+    client.run(os.environ['DISCORD_TOKEN'])
