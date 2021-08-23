414be3c5ad27fbfe176fed68b5746a5ad2a0932e
Andy Huang <73412379+goodrole@users.noreply.github.com>
2021-08-22 17:36:10 -0700

    updated files
    added pics and ui update

--- cogs/lolcommands.py
+++ cogs/lolcommands.py
@@ -1,4 +1,5 @@
 import discord
+from discord import file
 from discord.ext import commands
 from riotapi import *
 
@@ -9,30 +10,66 @@ class summoner_info(commands.Cog):
         self.client = client
 
     @commands.command()
-    async def clear(self, ctx, ammount=5):
-        await ctx.channel.purge(limit=ammount + 1)
-
-    @commands.command()
+    # adds summoner to the bot data
     async def register(self, ctx, *, arg):
+        if arg not in summoner_names:
 
-        register_summoner(arg)
+            file = discord.File('pics/thumbs_up.png', filename='image.png')
 
-        await ctx.send(f'{visible_summoner_names[-1]} has been registered.')
+            register_summoner(arg)
+
+            embed = discord.Embed(
+                colour=discord.Color.blue()
+            )
+            embed.set_author(
+                name=ctx.author.display_name,
+                icon_url=ctx.author.avatar_url
+            )
+            embed.add_field(
+                name='Register Summoner',
+                value=f'{visible_summoner_names[-1]} has been registered.')
+            embed.set_image(
+                url='attachment://image.png'
+            )
+
+            await ctx.send(file=file, embed=embed)
+        else:
+            await ctx.send(f'{arg} is aleady registered')
 
     @commands.command()
+    # shows stats of the summoner
     async def stats(self, ctx, *, arg):
         if arg in summoner_names:
+
+            file = discord.File('pics/ranked_stats.gif', filename='image.gif')
             print_stats(arg)
-            await ctx.send('\n'.join(visible_stats))
+
+            embed = discord.Embed(
+                colour=discord.Color.blue()
+            )
+            embed.add_field(
+                name='Ranked Stats',
+                value='\n'.join(visible_stats)
+            )
+            embed.set_image(
+                url='attachment://image.gif'
+            )
+
+            await ctx.send(file=file, embed=embed)
             visible_stats.clear()
         else:
             await ctx.send('The summoner is not registered')
 
     @commands.command()
+    # shows available chests
     async def chest(self, ctx, *, arg):
         if arg in summoner_names:
             hextech_chest(arg)
-            await ctx.author.send('\n'.join(available_chests))
+            await ctx.author.send(', '.join(available_chests))
+
+            # for teminal
+            print('champion id list has been cleared')
+            print('available chests list has been cleared')
             available_chests.clear()
         else:
             await ctx.send('The summoner is not registered')
--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import discord
 import os
 from discord.ext import commands
 
+token = 'ODc0NDYxMTE0NzYxNjEzMzcz.YRHTYA.hTqKfnhI0qtSlPywqzJGQFTuQ_8'
 client = commands.Bot(command_prefix='.')
 
 
@@ -9,25 +10,41 @@ client = commands.Bot(command_prefix='.')
 async def on_ready():
     print('bot ready')
 
+# clear dms
 
-# @client.command()
-# async def load(ctx, extension):  # ctx represents the context
-#    client.load_extension(f'cogs.{extension}')
-#
-#
-# @client.command()
-# async def unload(ctx, extension):
-#    client.load_extension(f'cogs.{extension}')
-#
-#
-# @client.command()
-# async def reload(ctx, extension):
-#    client.unload_extension(f'cogs.{extension}')
-#    client.load_extension(f'cogs.{extension}')
 
+@client.command()
+async def cleardm(ctx, user: discord.User):
+    await ctx.channel.purge(limit=1)
+    async for message in user.history():
+        if message.author == client.user:
+            await message.delete()
+
+# extension functions
+
+
+@client.command()
+async def load(ctx, extension):  # ctx represents the context
+    client.load_extension(f'cogs.{extension}')
+    print('loaded')
+
+
+@client.command()
+async def unload(ctx, extension):
+    client.load_extension(f'cogs.{extension}')
+    print('unloaded')
+
+
+@client.command()
+async def reload(ctx, extension):
+    client.unload_extension(f'cogs.{extension}')
+    client.load_extension(f'cogs.{extension}')
+    print('reloaded')
+
+# loads all the cog files
 for filename in os.listdir('./cogs'):
     if filename.endswith('.py'):
         client.load_extension(f'cogs.{filename[:-3]}')
 
-
-client.run(os.environ['DISCORD_TOKEN'])
+if __name__ == '__main__':
+    client.run(token)
